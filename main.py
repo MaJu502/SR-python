983dc1cc7cb5e489f66211904c840a27326cd632
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-10-26 16:42:06 -0600

    menu to select various camara angles

--- main.py
+++ main.py
@@ -8,3 +8,29 @@ r.glClear()
 r.LoadModel('./plant.obj', (5,4,1), (50,50,50), textureP=texture_pack)
 r.glFinish("out.bmp")
 r.glFinishZBuffer("outZBUFFED.bmp")
+
+def menu():
+    salir = False
+    while salir == False:
+
+        print("bienvenido! selecciona el angulo de camara que deseas generar:")
+        print("   1.  Medium Shot ")
+        print("   2.  Low Angle ")
+        print("   3.  High Angle ")
+        print("   4.  Dutch Angle ")
+        print("   5.  SALIR DE PROGRAMA ")
+
+        opcion = input()
+
+        if opcion == 1:
+            continue
+        elif opcion == 2:
+            continue
+        elif opcion == 3:
+            continue
+        elif opcion == 4:
+            continue
+        elif opcion == 5:
+            salir = True
+        else: 
+            print("opcion invalida porfavor intenta nuevamente")
